0ccd262482a13eef07b16a14f58af59e8fb8f60d
AdemcanReis <43937213+AdemcanReis@users.noreply.github.com>
2018-10-09 09:18:23 +0300

    Add files via upload

---
+++ komutlar/atatürk.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+var request = require('request');
+
+module.exports.run = (client, message, args) => {
+    request('https://api.eggsybot.xyz/ataturk', function (error, response, body) {
+    if (error) return console.log('Hata:', error); // Hata olursa, konsola göndersin,
+    else if (!error) { // Eğer hata yoksa;
+        var info = JSON.parse(body); // info değişkeninin içerisine JSON'ı ayrıştırsın,
+        message.channel.sendEmbed(new Discord.RichEmbed().setImage(info.link)); // ve konsola çıktıyı versin.
+    }
+})};
+
+module.exports.help = {
+  name: 'atatürk'
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['YEDEK KOMUT1', 'YEDEK KOMUT2'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "atatürk",
+  description: "Mustafa Kemal Paşa Fotoğrafını ATAR",
+  usage: "atatürk <mesaj>"
+};
