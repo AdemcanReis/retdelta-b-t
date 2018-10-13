98582d4f5414151608d7d9610b1a527e8f3bfefa
AdemcanReis <43937213+AdemcanReis@users.noreply.github.com>
2018-10-13 20:02:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,6 +84,26 @@ client.on('message', msg => {
   }
 });
 
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setActivity(`Yeni Güncellemeler Gelmiştir | rd!yardım`);
+});
+
+client.on("guildCreate", guild => {
+  //  when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Yeni Güncellemeler Gelmiştir | rd!yardım`);
+});
+
+client.on("guildDelete", guild => {
+  // when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Yeni Güncellemeler Gelmiştir | rd!yardım`);
+});
+
 ////////////////////////
 
 client.on("guildMemberAdd", member => {
